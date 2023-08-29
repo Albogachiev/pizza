06bfef9a9f65040a73e6dfd2813b20250f655a00
Gapur <110934827+Albogachiev@users.noreply.github.com>
2023-08-29 23:19:23 +0300

    подключил переход в компоненты cart, headerи и в случае если адрес не корректный, у нас будет рендарится компонент NotFound

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
+import { Cart } from './pages/Cart';
 
 function App() {
 
@@ -12,8 +14,11 @@ function App() {
     <Header />
     <div className="content">
       <div className="container">
-        <NotFound />
-        {/* <Home /> */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes> 
       </div>
     </div>
   </div>
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Cart() {
+  return (
+    <div>
+      <h1>DDDD</h1>
+    </div>
+  )
+}
